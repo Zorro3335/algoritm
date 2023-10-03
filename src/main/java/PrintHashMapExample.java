da954d03ede8f1af13b2db7ab9bd605557084bcb
Vasya2222 <116379970+Vasya2222@users.noreply.github.com>
2023-10-03 17:56:32 +0300

    add homework 4

---
+++ src/main/java/PrintHashMapExample.java
@@ -0,0 +1,12 @@
+public class PrintHashMapExample {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> hMapNumbers = new HashMap<Integer, String>();
+        hMapNumbers.put(1, "One");
+        hMapNumbers.put(2, "Two");
+        hMapNumbers.put(3, "Three");
+
+        System.out.println( hMapNumbers );
+
+    }
+}
